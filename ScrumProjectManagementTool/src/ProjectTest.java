5ffe4ed1350e9b2aa26cfd4925219fe1412777cb
NadiaJ <nadiajorgea@gmail.com>
2013-11-13 10:36:14 +0530

    First JunitTestCase(ProjectTest)

---
+++ ScrumProjectManagementTool/src/ProjectTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ProjectTest extends TestCase {
+
+	Project project;
+	@Before
+	public void setUp() throws Exception {
+		project=new Project();
+		project.setProjectName("Online Voting System");
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+		project=null;
+	}
+
+	@Test
+	public void testSetProjectName() {
+		assertEquals("Online Voting System",project.getProjectName());
+		fail("Not yet implemented");
+	}
+
+}
