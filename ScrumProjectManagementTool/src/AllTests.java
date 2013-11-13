01c59cc4fa672973061ce127d1593d4bba3041c2
NadiaJ <nadiajorgea@gmail.com>
2013-11-13 12:57:43 +0530

    Final1

---
+++ ScrumProjectManagementTool/src/AllTests.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ PersonTest.class, ProjectTest.class, TeamTest.class })
+public class AllTests {
+
+}
